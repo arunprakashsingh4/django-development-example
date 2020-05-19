92f456cdb4374f0a5084c852eb83b93b711dcd3c
arun <arunprakashsingh4@gmail.com>
2020-05-19 07:08:56 +0530

    first

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,12 @@
+# this file create by me(arun)
+
+from django.urls import path
+from basic_app import views
+
+app_name='basic_app'
+
+urlpatterns=[
+    path("relative/",views.relative,name="relative"),
+    path("other/",views.other,name="other"),
+
+]
